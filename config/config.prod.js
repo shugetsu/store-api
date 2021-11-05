c14ad4f83577c111c4ee2fe84e9f108052b076a0
shugetsu <1968304304@qq.com>
2021-11-05 19:16:35 +0800

    更新证书

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-06 00:20:38
+ * @LastEditTime: 2021-11-06 00:21:40
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \store-api\config\config.prod.js
+ */
 const path = require('path')
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -15,8 +23,8 @@ module.exports = appInfo => {
   // SSL
   config.cluster = {
     https: {
-      key: path.join(appInfo.baseDir, 'app/public/ssl/3801191_www.yu-yin.xin.key'),
-      cert: path.join(appInfo.baseDir, 'app/public/ssl/3801191_www.yu-yin.xin.pem')
+      key: path.join(appInfo.baseDir, 'app/public/ssl/6554519_www.yu-yin.xin.key'),
+      cert: path.join(appInfo.baseDir, 'app/public/ssl/6554519_www.yu-yin.xin.pem')
     }
   }
 
